5821517fc489ce839078f72234efeef66cab1b9f
CLawther <49128122+CLawther@users.noreply.github.com>
2021-03-11 22:25:47 +0000

    Camera Movement
    Added camera movement script to allow for a more smooth camera when following the player
    
    Currently works just not as intended need to fix later

---
+++ Colour Clash/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //declaring variables to be used later to enable less jittery camera movemnts for a better overall gameplay experience
+    public Transform goal;
+
+    public Vector3 offset;
+
+    [Range(0.01f, 1.0f)]
+    public float Smooth = 0.6f;
+
+    void Start()
+    {
+        offset = transform.position - goal.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = goal.position + offset;
+
+        transform.position = Vector3.Slerp(transform.position, desiredPosition, Smooth);
+    }
+}
