f7d72eaa4320bf9baaa67a6d2c5ec96e44c01fdc
CLawther <49128122+CLawther@users.noreply.github.com>
2021-08-17 15:55:31 +0100

    All Colour Pick Ups
    Created all colour pick ups and made sure they will change the players colour when player collides with right pick up to match the colour.

--- Colour Clash/Assets/Scripts/ColourChanger.cs
+++ Colour Clash/Assets/Scripts/ColourChanger.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class ColourChanger : MonoBehaviour
 {
-    //Reference to player
-    public GameObject Player;
     //Using a singleton other objects can access to match player's colour value
     public static ColourChanger instance;
 
@@ -15,21 +13,55 @@ public class ColourChanger : MonoBehaviour
     public string PlayerColour;
 
     //Integer to determine a value for player's colour
-    public int value;
+    public int colourvalue;
 
     private void Awake()
     {
         instance = this;
     }
 
-    private void OnTriggerEnter()
+    private void OnTriggerEnter( Collider Player)
     {
-        if(Player)
+        if(Player.CompareTag("GPickUp"))
         {
-            //Changing player's colour to green when it collides with the green pick up
+            //Changing player's colour to green when it collides with the green pick up using tags
             PlayerColour = "Green";
-            value = 1;
+            colourvalue = 1;
             rend.material.color = Color.green;
         }
+
+        if (Player.CompareTag("RPickUp"))
+        {
+            //Changing player's colour to red when it collides with the red pick up using tags
+            PlayerColour = "Red";
+            colourvalue = 2;
+            rend.material.color = Color.red;
+        }
+
+        if (Player.CompareTag("YPickUp"))
+        {
+            //Changing player's colour to yellow when it collides with the yellow pick up using tags
+            PlayerColour = "Yellow";
+            colourvalue = 3;
+            rend.material.color = Color.yellow;
+        }
+
+        if (Player.CompareTag("MPickUp"))
+        {
+            //Changing player's colour to magenta when it collides with the magenta pick up using tags
+            PlayerColour = "Magenta";
+            colourvalue = 4;
+            rend.material.color = Color.magenta;
+        }
+
+        if (Player.CompareTag("BPickUp"))
+        {
+            //Changing player's colour to blue when it collides with the blue pick up using tags
+            PlayerColour = "Blue";
+            colourvalue = 5;
+            rend.material.color = Color.blue;
+        }
+
+
     }
 }
