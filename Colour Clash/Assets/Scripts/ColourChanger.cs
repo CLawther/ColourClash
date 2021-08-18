1a7c793c1a1f5a518c0f3ab714b9da4cf699768c
CLawther <49128122+CLawther@users.noreply.github.com>
2021-08-18 17:40:01 +0100

    Trail Renderer
    Added Trail Renderer on player showing player is moving. This changes colour with the player as well.

--- Colour Clash/Assets/Scripts/ColourChanger.cs
+++ Colour Clash/Assets/Scripts/ColourChanger.cs
@@ -7,6 +7,8 @@ public class ColourChanger : MonoBehaviour
 
     public Renderer rend;
 
+    public TrailRenderer trail;
+
     //String used for player's colour
     public string PlayerColour;
 
@@ -22,42 +24,47 @@ public class ColourChanger : MonoBehaviour
     {
         if(Player.CompareTag("GPickUp"))
         {
-            //Changing player's colour to green when it collides with the green pick up using tags
+            //Changing player's colour to green when it collides with the green pick up using tags and its trail renderer
             PlayerColour = "Green";
             colourvalue = 1;
             rend.material.color = Color.green;
+            trail.material.color = Color.green;
         }
 
         if (Player.CompareTag("RPickUp"))
         {
-            //Changing player's colour to red when it collides with the red pick up using tags
+            //Changing player's colour to red when it collides with the red pick up using tags and its trail renderer
             PlayerColour = "Red";
             colourvalue = 2;
             rend.material.color = Color.red;
+            trail.material.color = Color.red;
         }
 
         if (Player.CompareTag("YPickUp"))
         {
-            //Changing player's colour to yellow when it collides with the yellow pick up using tags
+            //Changing player's colour to yellow when it collides with the yellow pick up using tags and its trail renderer
             PlayerColour = "Yellow";
             colourvalue = 3;
             rend.material.color = Color.yellow;
+            trail.material.color = Color.yellow;
         }
 
         if (Player.CompareTag("MPickUp"))
         {
-            //Changing player's colour to magenta when it collides with the magenta pick up using tags
+            //Changing player's colour to magenta when it collides with the magenta pick up using tags and its trail renderer
             PlayerColour = "Magenta";
             colourvalue = 4;
             rend.material.color = Color.magenta;
+            trail.material.color = Color.magenta;
         }
 
         if (Player.CompareTag("BPickUp"))
         {
-            //Changing player's colour to blue when it collides with the blue pick up using tags
+            //Changing player's colour to blue when it collides with the blue pick up using tags and its trail renderer
             PlayerColour = "Blue";
             colourvalue = 5;
             rend.material.color = Color.blue;
+            trail.material.color = Color.blue;
         }
     }
 }
