3ad31f0e38d77119d391640752d4acd81218d529
CLawther <49128122+CLawther@users.noreply.github.com>
2021-08-18 16:58:29 +0100

    Minor Changes
    Changed the glow settings to make the glow brighter and tweaked player's jump.

--- Colour Clash/Assets/Scripts/PlayerMovement.cs
+++ Colour Clash/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     public float gravity = 30.0f;
 
     private Vector3 moveDirection = Vector3.zero;
+    private bool isGrounded;
 
     void Start()
     {
@@ -18,17 +19,23 @@ public class PlayerMovement : MonoBehaviour
         player = GetComponent<CharacterController>();
     }
 
+    private void OnCollisionStay()
+    {
+        //Setting grounded to true
+        isGrounded = true;
+    }
+
     void Update()
     {
-        //If player is grounded player can jump
+        //If player is grounded enabling vertical and horizontal movement
         if (player.isGrounded)
         {
             //Using Unity's built on axis so player can move with WASD or arrow keys
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
             moveDirection *= -movespeed;
 
-            //When spacebar is pressed player jumps
-            if (Input.GetKeyDown(KeyCode.Space))
+            //When spacebar is pressed player jumps and if player is grounded
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
             {
                 //Adding jump power when player jumps
                 moveDirection.y = jumppower;
